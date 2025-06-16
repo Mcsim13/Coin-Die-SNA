02f1354978e591cefd2405eaf605e342c8d1e6e9
Mcsim <simon130803@web.de>
2025-06-16 22:08:24 +0200

    Graph construction update

--- cluster_to_graph.py
+++ cluster_to_graph.py
@@ -1,8 +1,11 @@
 import json
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def get_findspots():
+    """Returns { findspot: coin_count } dict"""
     findspots = dict()
     with open("bushel_series_DataChallenge_2025-numisdata4-2_TypenEinzeln.csv", "r") as numisdata_file:
         _ = next(numisdata_file)
@@ -14,6 +17,7 @@ def get_findspots():
 
 
 def get_coin_findspots():
+    """Returns { coind_id: findspot } dict"""
     coin_findspots = {}
     with open("bushel_series_DataChallenge_2025-numisdata4-2_TypenEinzeln.csv", "r") as numisdata_file:
         _ = next(numisdata_file)
@@ -25,6 +29,7 @@ def get_coin_findspots():
 
 
 def imagecluster_get_cluster(cluster_json):
+    """Returns { cluster_id : [coin_id] } from { coin_id: cluster_id } input json"""
     with open(cluster_json, "r") as cluster_file:
         cluster_raw = json.load(cluster_file)
 
@@ -36,6 +41,11 @@ def imagecluster_get_cluster(cluster_json):
 
 
 def map_clusters_to_findspots(clusters, coin_findspots):
+    """
+    Returns { cluster_id: { finspot: [coin_id] } }
+    clusters: { cluster_id : [coin_id] }
+    coin_findspots: { coind_id: findspot }
+    """
     clusters_at_findspot = {}
     for cluster, coins in clusters.items():
         coins_at_fs = {}
@@ -56,11 +66,12 @@ def connect_nodes_pairwise(node_list, name_prefix=""):
     return edges
 
 
-"""
-Nodes = Cluster at Findspot
-Edges = Nodes with same cluster
-"""
 def imagecluster_to_gephi_graph_reduced(node_csv, edge_csv):
+    """
+    OLD
+    Nodes = Cluster at Findspot
+    Edges = Nodes with same cluster
+    """
     clusters = imagecluster_get_cluster("cluster_result_rueckseite.json")
     coin_findspots = get_coin_findspots()
 
@@ -100,15 +111,16 @@ def imagecluster_to_gephi_graph_reduced(node_csv, edge_csv):
     return clusters_at_findspot, nodes, edges
 
 
-"""
-Edges = Same findspot
-"""
 def findspot_edges_gephi(findspot_nodes, findspot_edges_csv):
+    """
+    OLD
+    Edges = Same findspot
+    """
     # nodes_flat = [node[0] for node in findspot_nodes]
     edges = []
     for i in range(len(findspot_nodes)):
-        for k in range(i+1, len(findspot_nodes)):
-            if (findspot_nodes[i][2] == findspot_nodes[k][2]):
+        for k in range(i + 1, len(findspot_nodes)):
+            if findspot_nodes[i][2] == findspot_nodes[k][2]:
                 edges.append((findspot_nodes[i][0], findspot_nodes[k][0]))
 
     print(len(edges), "Findspot Edges")
@@ -122,11 +134,12 @@ def findspot_edges_gephi(findspot_nodes, findspot_edges_csv):
             writer.writerow([edge[0], edge[1], "Undirected", "findspot"])
 
 
-"""
-Nodes = Coins
-Edges = Same Cluster
-"""
 def imagecluster_to_gephi_graph(cluster_json, node_csv, edge_csv):
+    """
+    OLD
+    Nodes = Coins
+    Edges = Same Cluster
+    """
     with open(cluster_json, "r") as cluster_file:
         cluster_raw = json.load(cluster_file)
 
@@ -166,17 +179,17 @@ def imagecluster_to_gephi_graph(cluster_json, node_csv, edge_csv):
             writer.writerow([edge[0], edge[1], "Undirected"])
 
 
-"""
-Nodes = Cluster, Findspots
-Edges = Coin of Cluster found at findspot 
-"""
-def imagecluster_to_gephi_graph_v3(node_csv, edge_csv):
-    clusters = imagecluster_get_cluster("cluster_result_rueckseite.json")
+def construct_graph(cluster_file, node_csv, edge_csv):
+    """
+    Nodes = Cluster, Findspots
+    Edges = Coin of Cluster found at findspot
+    """
+    clusters = imagecluster_get_cluster(cluster_file)
     coin_findspots = get_coin_findspots()
     findspots = get_findspots()
-    #print(clusters)
-    #print(coin_findspots)
-    print(findspots)
+    # print(clusters)
+    # print(coin_findspots)
+    # print(findspots)
 
     # Nodes
     clusters_at_findspot = map_clusters_to_findspots(clusters, coin_findspots)
@@ -216,11 +229,65 @@ def imagecluster_to_gephi_graph_v3(node_csv, edge_csv):
         for edge in edges:
             writer.writerow([edge[0], edge[1], "Undirected", edge[2]])
 
+    return nodes, edges
+
+
+def networkX_graph():
+    G = nx.Graph()
+
+    nodes, edges = construct_graph("rsc/die_studie_reverse_7_projhdbscan.json", "graphlists/die_studie_reverse_nodes.csv", "graphlists/die_studie_reverse_edges.csv")
+
+    node_names = [node[0] for node in nodes]
+    edges_only = [edge[:2] for edge in edges]
+
+    G.add_nodes_from(node_names)
+    G.add_edges_from(edges_only)
+
+    print(G)
+
+    data1 = nx.node_link_data(G, edges="edges")
+    # print(data1)
+    json_graph = json.dumps(data1, indent=4)
+
+    with open("networkx_export.json", "w") as f:
+        f.write(json_graph)
+
+
+def plot_coint_per_die(clusters):
+    length_counts = {}
+
+    for coins in clusters.values():
+        length = len(coins)
+        if length in length_counts:
+            length_counts[length] += 1
+        else:
+            length_counts[length] = 1
+
+    min_len = min(length_counts.keys())
+    max_len = max(length_counts.keys())
+
+    lengths = list(range(min_len, max_len + 1))
+    counts = [length_counts.get(length, 0) for length in lengths]
+
+    plt.bar(lengths, counts)
+    plt.xlabel("Coins per die")
+    plt.ylabel("Count")
+    plt.xticks(lengths)
+    plt.grid(axis="y", linestyle="-", alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     # imagecluster_to_gephi_graph("cluster_result_rueckseite.json", "imagecluster_reverse_nodes.csv", "imagecluster_reverse_edges.csv")
 
     # clusters_at_findspot, nodes, edges = imagecluster_to_gephi_graph_reduced("imagecluster_reverse_nodes.csv", "imagecluster_reverse_edges.csv")
     # findspot_edges_gephi(nodes, "imagecluster_reverse_findspot_edges.csv")
+    # -----------
 
-    imagecluster_to_gephi_graph_v3("imagecluster_reverse_nodes.csv", "imagecluster_reverse_edges.csv")
+    # construct_graph("rsc/die_studie_reverse_7_projhdbscan.json", "imagecluster_reverse_nodes.csv", "imagecluster_reverse_edges.csv")
+
+    # networkX_graph()
+
+    clusters = imagecluster_get_cluster("rsc/die_studie_obverse_8_projhdbscan.json")
+    plot_coint_per_die(clusters)
